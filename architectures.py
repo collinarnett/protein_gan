fixed architecture and added down-scaling
- added LeakyReLU as 'inplace' as it was not applying before
- set bias to false for all convolutional layers
- added downscaling in train.py as specified in the paper

architectures.py
@@ -7,18 +7,24 @@ class Generator16(nn.Module):
         super(Generator16, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            nn.ConvTranspose2d(100, 512, kernel_size=4, stride=1, padding=0),
+            nn.ConvTranspose2d(
+                100, 512, kernel_size=4, stride=1, padding=0, bias=False
+            ),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(
+                512, 256, kernel_size=3, stride=1, padding=1, bias=False
+            ),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(
+                256, 128, kernel_size=4, stride=2, padding=1, bias=False
+            ),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 1, kernel_size=3, stride=1, padding=1),
+            nn.ConvTranspose2d(64, 1, kernel_size=3, stride=1, padding=1, bias=False),
         )
 
     def forward(self, input):
@@ -30,27 +36,28 @@ class Discriminator16(nn.Module):
         super(Discriminator16, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
-            nn.LeakyReLU(0.2),
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0),
-            nn.Sigmoid()
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid(),
         )
 
     def forward(self, input):
         return self.main(input)
+
 
 # 64x64
 class Generator64(nn.Module):
@@ -58,18 +65,24 @@ class Generator64(nn.Module):
         super(Generator64, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            nn.ConvTranspose2d(100, 512, kernel_size=4, stride=1, padding=0),
+            nn.ConvTranspose2d(
+                100, 512, kernel_size=4, stride=1, padding=0, bias=False
+            ),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(
+                512, 256, kernel_size=4, stride=2, padding=1, bias=False
+            ),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(
+                256, 128, kernel_size=4, stride=2, padding=1, bias=False
+            ),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1, bias=False),
         )
 
     def forward(self, input):
@@ -81,27 +94,28 @@ class Discriminator64(nn.Module):
         super(Discriminator64, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
+            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0),
-            nn.Sigmoid()
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid(),
         )
 
     def forward(self, input):
         return self.main(input)
+
 
 # 128x128
 class Generator128(nn.Module):
@@ -109,18 +123,24 @@ class Generator128(nn.Module):
         super(Generator128, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            nn.ConvTranspose2d(100, 512, kernel_size=4, stride=4, padding=0),
+            nn.ConvTranspose2d(
+                100, 512, kernel_size=4, stride=4, padding=0, bias=False
+            ),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(
+                512, 256, kernel_size=4, stride=2, padding=1, bias=False
+            ),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=4, padding=0),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(
+                256, 128, kernel_size=4, stride=4, padding=0, bias=False
+            ),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
-            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1, bias=False),
         )
 
     def forward(self, input):
@@ -132,23 +152,23 @@ class Discriminator128(nn.Module):
         super(Discriminator128, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
+            nn.Conv2d(1, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(128, 256, kernel_size=4, stride=4, padding=1),
+            nn.Conv2d(128, 256, kernel_size=4, stride=4, padding=1, bias=False),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
+            nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.1),
-            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0),
-            nn.Sigmoid()
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid(),
         )
 
     def forward(self, input):

train.py
@@ -50,6 +50,9 @@ def train(batch_size, num_epochs, lr, manualSeed, image_size, data_path):
         x = data_file[f"train_{image_size}"][:]
     data_len = len(x)
 
+    # Scale down values by 100
+    x = (x * 1) / 100
+
     # setting device on GPU if available, else CPU
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -232,7 +235,11 @@ def train(batch_size, num_epochs, lr, manualSeed, image_size, data_path):
                     fake = netG(fixed_noise).detach().cpu()
                 writer.add_image(
                     "Generator Output",
-                    vutils.make_grid(fake, padding=2, normalize=True),
+                    vutils.make_grid(
+                        (fake.clamp(min=0) + fake.clamp(min=0).permute(0, 1, 3, 2)) / 2,
+                        padding=2,
+                        normalize=True,
+                    ),
                     iters,
                 )
 